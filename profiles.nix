92d71288f958d413e2b8b84ee53e0aca1208f16a
zhongjis <23202735+zhongjis@users.noreply.github.com>
2026-08-05 14:33:24 -0700

    feat(nix): add profiles.nix profile map

---
+++ profiles.nix
@@ -0,0 +1,14 @@
+{
+  personal = [
+    "recharts-patterns"
+    "supabase-postgres-best-practices"
+    "svelte"
+    "sveltekit"
+  ];
+  work = [
+    "enterprise-scala"
+    "github-pr-management"
+    "mysql-best-practices"
+    "splunk"
+  ];
+}
